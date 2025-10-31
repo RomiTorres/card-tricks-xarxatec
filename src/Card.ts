71d1f1332c9bd89b39e4880ced0b74ca30d833fb
RomiTorres <torres_rominav@hotmail.com>
2025-10-31 18:11:13 +0100

    Resuelto con código de Jano

--- src/Card.ts
+++ src/Card.ts
@@ -1,17 +1,54 @@
 export class Card {
+  #rank: string;
+  #suit: string;
+  #isFaceUp: boolean;
 
-  spinCard():void {
+  constructor(rank:string, suit: string) {
+    this.#rank = rank;
+    this.#suit = suit;
+    this.#isFaceUp = false;
+  }
+
+  get rank(): string {
+    return this.#rank;
+  }
+
+  get suit(): string {
+    return this.#suit;
+  }
+
+  get isFaceUp(): boolean {
+    return this.#isFaceUp;
+  }
+
+  set isFaceUp(newState: boolean) {
+    this.#isFaceUp = newState;
+  }
+
+  toggleFace(): void {
+    if(this.#isFaceUp) this.#isFaceUp = false ;
+        else this.#isFaceUp = true; 
+  }
+
+  setCardImage(id:string) {
+    const card = document.getElementById(id);
+    let imagePath = "../public/img/back.png";
+    if(this.#isFaceUp) imagePath = "../public/img/AS.png"
+    card.setAttribute("src", imagePath);
+  }
+
+  // spinCard():void {
     
-    const currentLetter = document.getElementById("reverse-letter");
-    currentLetter.addEventListener("click", () => {
-      currentLetter.remove();
-      console.log(currentLetter, 'soy current')
-    const newCard = document.createElement("img");
-    newCard.src = './img/AS.png';
-    const container = document.querySelector("p");
-    container.appendChild(newCard);
+  //   const currentLetter = document.getElementById("reverse-letter");
+  //   currentLetter.addEventListener("click", () => {
+  //     currentLetter.remove();
+  //     console.log(currentLetter, 'soy current')
+  //   const newCard = document.createElement("img");
+  //   newCard.src = './img/AS.png';
+  //   const container = document.querySelector("p");
+  //   container.appendChild(newCard);
   
-  })}
+  // })}
 
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Card } from "./Card.js";
 
-const cardInstance = new Card();
+// const cardInstance = new Card();
 
-cardInstance.spinCard();
+// cardInstance.spinCard();
 
 
+const card = new Card("A", "spades");
+document.getElementById("reverse-letter").addEventListener("click", (e) => {
+  card.toggleFace();
+  console.log(card)
+  if(e.target instanceof HTMLElement)  card.setCardImage(e.target.getAttribute("id"));
+});
