1f2570fb9b1b614395aee27c67fc4f1937aa70b2
RomiTorres <torres_rominav@hotmail.com>
2025-11-06 10:11:23 +0100

    Dos cartas funcionando, anv y rev

---
+++ src/Board.ts
@@ -0,0 +1,35 @@
+import { Card } from "./Card.js";
+
+export class Board {
+  #numberOfVisbleCards: number;
+  #visibleCards: Map<number, Card> 
+
+  constructor(deck:Array<Card>, numberOfVisbleCards: number ) {
+    this.#numberOfVisbleCards = numberOfVisbleCards;
+    this.#visibleCards = new Map<number, Card> 
+    const deckCopy: Array<Card> = deck.map((card) => {return new Card(card.rank, card.suit)});
+    for(let i = 1; i <= numberOfVisbleCards; i++) {
+      const randomNumberCard = Math.floor(Math.random() * deckCopy.length);
+      this.#visibleCards.set(i, deckCopy[randomNumberCard]);
+      deckCopy.splice(randomNumberCard, 1);
+    }
+    
+    
+    deck.forEach((card) => {
+
+    })
+  }
+
+  get visibleCards():Map<number, Card> {
+    return this.#visibleCards;
+  }
+
+  renderBoard() {
+    const boardCards = document.getElementsByClassName("deck-card");
+    for(let i = 0; i < boardCards.length; i++) {
+      if( this.#visibleCards.get(i+1) instanceof Card){
+        this.#visibleCards.get(i+1).setCardImage(`card-${i+1}`);
+      } 
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Board } from "./Board.js";
 import { Card } from "./Card.js";
 
 // const cardInstance = new Card();
@@ -7,14 +8,22 @@ import { Card } from "./Card.js";
 
 const card1 = new Card("A", "spades"); // spades, hearts, diamonds, clubs
 const card2 = new Card("K", "hearts")
-document.getElementById("first-card").addEventListener("click", (e) => {
-  card1.toggleFace();
-  console.log(card1)
-  if(e.target instanceof HTMLElement)  card1.setCardImage(e.target.getAttribute("id"));
+const card3 = new Card("3", "clubs");
+const card4 = new Card("9", "diamonds");
+
+const deck = [card1, card2, card3, card4];
+const board = new Board(deck, 2);
+board.renderBoard();
+document.getElementById("card-1").addEventListener("click", (e) => {
+  if( board.visibleCards.get(1) instanceof Card) {
+  board.visibleCards.get(1).toggleFace();
+  if(e.target instanceof HTMLElement)  board.visibleCards.get(1).setCardImage(e.target.getAttribute("id"));
+  }
 });
 
-document.getElementById("second-card").addEventListener("click", (e) => {
-  card2.toggleFace();
-  console.log(card2)
-  if(e.target instanceof HTMLElement)  card2.setCardImage(e.target.getAttribute("id"));
+document.getElementById("card-2").addEventListener("click", (e) => {
+   if( board.visibleCards.get(2) instanceof Card) {
+  board.visibleCards.get(2).toggleFace();
+  if(e.target instanceof HTMLElement)  board.visibleCards.get(2).setCardImage(e.target.getAttribute("id"));
+   }
 });
